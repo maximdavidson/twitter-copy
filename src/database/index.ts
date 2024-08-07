db4c2e2ebff877db8fd9a03f01e2b36524d7b3b7
Maxim Davidson <maxd02122004@gmail.com>
2024-08-07 18:37:47 +0300

    fix: Big bug with env

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,14 +7,24 @@ import {
   signInWithPopup,
 } from 'firebase/auth';
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAU3xiBKNKJh-HX23YKgEL8HytUDc225tE',
+  authDomain: 'modsentwitter.firebaseapp.com',
+  databaseURL: 'https://modsentwitter-default-rtdb.firebaseio.com',
+  projectId: 'modsentwitter',
+  storageBucket: 'modsentwitter.appspot.com',
+  messagingSenderId: '357057773191',
+  appId: '1:357057773191:web:212dc61b163865b3170222',
 };
 
 const app = initializeApp(firebaseConfig);
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env': {
-      VITE_FIREBASE_API_KEY: '"AIzaSyAU3xiBKNKJh-HX23YKgEL8HytUDc225tE"',
-      VITE_FIREBASE_AUTH_DOMAIN: '"modsentwitter.firebaseapp.com"',
-      VITE_FIREBASE_DATABASE_URL: '"https://modsentwitter-default-rtdb.firebaseio.com"',
-      VITE_FIREBASE_PROJECT_ID: '"modsentwitter"',
-      VITE_FIREBASE_STORAGE_BUCKET: '"modsentwitter.appspot.com"',
-      VITE_FIREBASE_MESSAGING_SENDER_ID: '"357057773191"',
-      VITE_FIREBASE_APP_ID: '"1:357057773191:web:212dc61b163865b3170222"',
-    },
-  },
   resolve: {
     alias: {
       components: resolve(__dirname, 'src/components'),
